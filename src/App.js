ca527dd4dcbe1d843e8ac6c9320d43fbc33286c4
pankaj ram pawar <pankajrampawar@pankajs-MacBook-Pro-2.local>
2023-11-21 20:59:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
 
 function App() {
+
+  const [user, setUser] = React.useState(null)
+
+  React.useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='flex justify-center pt-36 bg-gray-100 h-screen w-screen'>
+        {user && user.results && user.results.length > 0 && (
+        
+        <div className='bg-white flex gap-5 p-6 rounded-3xl shadow-xl h-fit'>
+          <div>
+            <img src={user.results[0].picture.large} alt='user' className='rounded-2xl h-44'/>
+          </div>
+          <div className=' flex flex-col justify-evenly text font-mono font-semibold text-xl'>
+            <div className='flex gap-4'><span>{user.results[0].name.first}</span> <span>{user.results[0].name.last}</span></div>
+            <p>{user.results[0].gender}</p>
+            <p>{user.results[0].phone}</p>
+          </div>
+        </div>
+        )}
+  </div>
   );
 }
 
